685c839c6f9d19575ca8ab240266da79290f2deb
FARELNICKHOLAS <farel.harri@gmail.com>
2025-02-05 22:46:52 +0800

    adding fonts

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,13 +7,16 @@
     <title>Bejobalitour | About Us</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 </head>
-<body class="relative pt-20 ">
+<body class="relative pt-20">
     @include('components.navbar')
-    <div class="flex items-center h-screen">
+    <div class="flex h-screen bg-blue-100 items-center">
     <div class="text-center mx-auto text-white">
-      <h1 class="text-6xl font-semibold text-black"><a class="text-green-500">About</a> Us<h1>
+      <h1 class="text-5xl font-semibold text-black">Who are we?<h1>
     </div>
   </div>
     @include('components.footer')
